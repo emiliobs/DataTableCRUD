026d376ec2eb05d7c158f1156547a5ef6631f358
Emilio Barrera <barrera_emilio@hotmail.com>
2016-12-15 13:15:39 +0100

    CRUD with datable of jquey and bootstrap

---
+++ DataTableCRUD/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataTableCRUD.Models;
+
+namespace DataTableCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployee()
+        {
+            using (MyDatabaseEntities db = new MyDatabaseEntities() )
+            {
+                var employees= db.Employees.OrderBy(a => a.FirstName).ToList();
+
+                return Json(new {data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
