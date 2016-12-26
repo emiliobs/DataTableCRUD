604e0e571f06e36e497c563815ac01804d1530a4
Emilio Barrera <barrera_emilio@hotmail.com>
2016-12-26 21:47:40 +0100

    CRUD in DataTable con asps.net mvc y sqlserver 2016, bootstrap, query , ventanas modales con poput y ajax, y mas = ok

--- DataTableCRUD/Controllers/HomeController.cs
+++ DataTableCRUD/Controllers/HomeController.cs
@@ -15,14 +15,109 @@ namespace DataTableCRUD.Controllers
             return View();
         }
 
+
+        [HttpGet]
         public ActionResult GetEmployee()
+        {
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
+            {
+                var employees = db.Employees.OrderBy(a => a.FirstName).ToList();
+
+                return Json(new {data = employees}, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
+            {
+                var v = db.Employees.FirstOrDefault(a => a.EmployeeId == id);
+                return View(v);
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee employee)
+        {
+            bool status = false;
+
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities db = new MyDatabaseEntities())
+                {
+                    if (employee.EmployeeId > 0)
+                    {
+
+
+                        //Edit:
+                        var v = db.Employees.FirstOrDefault(e => e.EmployeeId == employee.EmployeeId);
+
+                        if (v != null)
+                        {
+                            v.City = employee.City;
+                            v.Country = employee.Country;
+                            v.Email = employee.Email;
+                            v.City = employee.City;
+                            v.LastName = employee.LastName;
+                        }
+
+
+                    }
+                    else
+                    {
+                        //Save
+                        db.Employees.Add(employee);
+                    }
+
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+
+            return new JsonResult {Data = new {status = status}};
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
         {
             using (MyDatabaseEntities db = new MyDatabaseEntities() )
             {
-                var employees= db.Employees.OrderBy(a => a.FirstName).ToList();
+                var v = db.Employees.FirstOrDefault(a => a.EmployeeId == id);
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
 
-                return Json(new {data = employees }, JsonRequestBehavior.AllowGet);
             }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+
+            using (MyDatabaseEntities db = new MyDatabaseEntities() )
+            {
+                var v = db.Employees.FirstOrDefault(a => a.EmployeeId == id);
+
+                if (v != null)
+                {
+                    db.Employees.Remove(v);
+                    db.SaveChanges();
+
+                    status = true;
+                }
+            }
+
+            return  new JsonResult { Data = new { status = status}};
         }
     }
 }
---
+++ DataTableCRUD/Models/Extended/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataTableCRUD.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+
+    public class EmployeeMetadata
+    {
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "Please provide first Name.")]
+        [Required]
+        public string FirstName { get; set; }
+
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Last Name.")]
+        [Required]
+        public string LastName { get; set; }
+
+    }
+}
